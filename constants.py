2e343ce48e88fbdfd07ff5fe47e860b02e40927e
Minigrim0 <grimauflorent@gmail.com>
2023-05-03 17:40:24 +0200

    [Fix] Size + microcontroller

--- constants.py
+++ constants.py
@@ -4,15 +4,15 @@ UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
-DIM = 20
-SIZE = 45
+DIM = 10
+SIZE = 40
 SHOW_ENTROPY = False
 SPEED = 10
 STEP_BY_STEP = False
 
 
 pg.init()
-window = pg.display.set_mode((900, 900))
+window = pg.display.set_mode((400, 400))
 
 empty_image = pg.Surface((SIZE, SIZE))
 empty_image.fill((255, 255, 255))
